63177efd0f5db8f2b492030ee0e85adf573c2f1a
Kawsar Bin Siraj <38612699+kawsarBinSiraj@users.noreply.github.com>
2021-11-24 12:36:45 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+
+import './App.scss';
+import React from 'react'
+
+const RootComponent = () => {
+  return (
+    <>
+      <div id="RootComponent" style={{ textAlign: 'center'}}>
+        <a href="https://kawsarbinsiraj.github.io/" target="_blank" id="app">
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+          <div className="app-widget"></div>
+        </a>
+        <h2 style={{color : '#fff'}}>Animation created with only html & css throw React js</h2>
+      </div>
+    </>
+  )
+}
+
+export default RootComponent
